799b50f5e5728241cc00efd90c66cbb7314a3322
Ivan Argandoña <ivan.argandona.2378@gmail.com>
2024-09-24 01:15:07 -0600

    Conexion a la base de datos realizada

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyB5ZeYazet5e0QFO8bsLlopDX63BcBn3qk",
+    authDomain: "cheesetrack-dd8bc.firebaseapp.com",
+    projectId: "cheesetrack-dd8bc",
+    storageBucket: "cheesetrack-dd8bc.appspot.com",
+    messagingSenderId: "255709776623",
+    appId: "1:255709776623:web:f022f32d6c0903cf66e91a",
+    measurementId: "G-R7V8XREJT9"
+  }
 };
 
 /*
