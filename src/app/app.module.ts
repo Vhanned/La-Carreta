20fe60c44622fdea43946177aac739ef11939c18
Ivan Argandoña <ivan.argandona.2378@gmail.com>
2024-09-24 02:46:56 -0600

    Ya funciona el firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
-//Base de datos
-
+// Base de datos
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore'; 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './Componentes/login/login.component';
 
-
-
+// Import your environment (make sure it's the right path)
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -20,9 +21,9 @@ import { LoginComponent } from './Componentes/login/login.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
-    //provideFirebaseApp(()=>initializeApp(environment.firebaseConfig)),
-    //provideFirestore(()=>getFirestore())
+    FormsModule,
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
