49d4968e30fa14c7963388dcd248c44a24efa01c
Vhanned <ivan.argandona.2378@gmail.com>
2024-10-14 00:07:43 -0600

    mevboyamatar`(*>﹏<*)′

--- src/app/Componentes/inventarios-productos/inventarios-productos.component.ts
+++ src/app/Componentes/inventarios-productos/inventarios-productos.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { collection, Firestore } from '@angular/fire/firestore';
+import { collection, collectionData, doc, Firestore, query, setDoc } from '@angular/fire/firestore';
 import { Producto } from 'src/app/clases/clases.component';
 
 @Component({
@@ -16,15 +16,58 @@ export class InventariosProductosComponent {
   listaProductos: Producto[] = new Array();
 
   //Direccion en donde se guardan los productos en firebase
-  ProductosBD = collection(this.firebase, "MateriasPrimas");
+  ProductosBD = collection(this.firebase, "Productos");
 
-  constructor(private firebase:Firestore) { 
+  constructor(private firebase: Firestore) {
+
+    let q = query(this.ProductosBD);
+    collectionData(q).subscribe((productoSnap) => {
+      this.listaProductos = new Array();
+      productoSnap.forEach((item) => {
+        let materiaPrima = new Producto();
+        materiaPrima.setData(item);
+        console.log(item);
+        this.listaProductos.push(materiaPrima);
+      });
+    });
 
 
   }
 
-  
+  insertarProducto() {
+
+    this.nuevoProducto.Id_Producto = this.GenerateRandomString(20); // Generar un ID único para la nueva materia
+    let nuevaMateriaDoc = doc(this.firebase, "Productos", this.nuevoProducto.Id_Producto);
+
+    // Guardar la nueva materia en Firestore
+    setDoc(nuevaMateriaDoc, JSON.parse(JSON.stringify(this.nuevoProducto)))
+      .then(() => {
+        alert("Producto agregado exitosamente");
+        this.limpiarFormulario();
+      })
+      .catch((error) => {
+        console.error("Error al agregar el producto: ", error);
+      });
+
+    let btnCerrar = document.getElementById('btnCerrarModalElemento');
+    btnCerrar?.click();
+
+  }
 
 
+  GenerateRandomString(num: number): string {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+    const charactersLength = characters.length;
+    for (let i = 0; i < num; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+  }
+
+
+  limpiarFormulario() {
+    this.nuevoProducto = new Producto(); // Resetea la nueva materia
+  }
 
 }
