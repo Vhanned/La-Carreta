84066e71a895f50f68af3066c2efb2dc6e8cfca5
Christo-25 <a174310@une.edu.mx>
2024-10-18 02:24:46 -0600

    Pantallas creadas de amd-cont, produccion, ventas, almacen, agrege las rutas en el app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,10 @@ import { MenuprincipalComponent } from './Componentes/menuprincipal/menuprincipa
 import { InventariosComponent } from './Componentes/inventarios/inventarios.component';
 import { OrdenesProduccionComponent } from './Componentes/ordenes-produccion/ordenes-produccion.component';
 import { InventariosProductosComponent } from './Componentes/inventarios-productos/inventarios-productos.component';
+import { AlmacenComponent } from './Componentes/almacen/almacen.component';
+import { AdmContComponent } from './Componentes/adm-cont/adm-cont.component';
+import { ProduccionComponent } from './Componentes/produccion/produccion.component';
+import { VentasComponent } from './Componentes/ventas/ventas.component';
 
 const routes: Routes = [
 {path:'',component:LoginComponent},
@@ -12,7 +16,11 @@ const routes: Routes = [
 {path:'menuprincipal',component:MenuprincipalComponent},
 {path: 'inventarios',component:InventariosComponent},
 {path: 'ordenes-produccion', component:OrdenesProduccionComponent},
-{path: 'inventarios-productos', component:InventariosProductosComponent}
+{path: 'inventarios-productos', component:InventariosProductosComponent},
+{path: 'c',component:AlmacenComponent},
+{path: 's', component:AdmContComponent},
+{path: 'XD', component:ProduccionComponent},
+{path: ':v', component:VentasComponent}
 ];
 
 @NgModule({
